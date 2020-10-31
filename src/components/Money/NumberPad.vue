df12b94feef2bf430544368ac73ae4616d0e6e6a
huangdi <huangdi_c@163.com>
2020-10-31 15:04:36 +0800

    用JS实现一个组件

--- src/components/Money/NumberPad.vue
+++ src/components/Money/NumberPad.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="numberPad">
-    <div class="output">100</div>
+    <div class="output">{{ output }}</div>
     <div class="buttons">
-      <button>1</button>
+      <button @click="output+=1">1</button>
       <button>2</button>
       <button>3</button>
       <button>删除</button>
@@ -21,9 +21,13 @@
 </template>
 
 <script lang="ts">
-export default {
-  name: 'NumberPad'
-};
+import Vue from 'vue';
+import {Component} from 'vue-property-decorator';
+
+@Component
+export default class NumberPad extends Vue {
+  output = '';
+}
 </script>
 
 <style lang="scss" scoped>
