0852f88c3c63fb01c3dac450c352c7394028313b
huangdi <huangdi_c@163.com>
2020-11-02 22:46:35 +0800

    修改

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,14 @@
+/* eslint-disable */
 const path = require('path')
 
 module.exports = {
   lintOnSave: false,
+  devServer: {
+    overlay: {
+      warnings: true,
+      errors: true
+    }
+  },
   chainWebpack: config =>{
     const dir = path.resolve(__dirname, 'src/assets/icons')
 
