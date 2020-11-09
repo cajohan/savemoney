b73670aa40fcc50587e03de04024945c21a78248
huangdi <huangdi_c@163.com>
2020-11-09 15:46:32 +0800

    使用全局tagList

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -18,18 +18,17 @@ import Types from '@/components/Money/Types.vue';
 import Tags from '@/components/Money/Tags.vue';
 import {Component, Watch} from 'vue-property-decorator';
 import recordListModel from '@/models/recordListModel';
-import tagListModel from '@/models/tagListModel';
 import FormItem from '@/components/Money/FormItem.vue';
 
 const recordList = recordListModel.fetch();
-const tagList = tagListModel.fetch();
+
 
 
 @Component({
   components: {FormItem, Tags, Types, NumberPad}
 })
 export default class Money extends Vue {
-  tags = tagList;
+  tags = window.tagList;
   recordList: RecordItem[] = recordList;
   record: RecordItem = {
     tags: [], notes: '', type: '-', amount: 0
