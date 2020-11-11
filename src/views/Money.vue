361334ee2eeda8435a227a1382a5a30eaf3c873d
huangdi <huangdi_c@163.com>
2020-11-11 18:05:16 +0800

    use Vuex

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -18,15 +18,19 @@ import Types from '@/components/Money/Types.vue';
 import Tags from '@/components/Money/Tags.vue';
 import {Component} from 'vue-property-decorator';
 import FormItem from '@/components/Money/FormItem.vue';
-import store from '@/store/index2';
+import oldStore from '@/store/index2';
 
 
 @Component({
-  components: {FormItem, Tags, Types, NumberPad}
+  components: {FormItem, Tags, Types, NumberPad},
+  computed:{
+    recordList(){
+      return this.$store.state.count;
+    }
+  }
 })
 export default class Money extends Vue {
-
-  recordList = store.recordList;
+  recordList = oldStore.recordList;
   record: RecordItem = {
     tags: [], notes: '', type: '-', amount: 0
   };
@@ -36,7 +40,7 @@ export default class Money extends Vue {
   }
 
   saveRecord() {
-    store.createRecord(this.record);
+    oldStore.createRecord(this.record);
   }
 }
 </script>
