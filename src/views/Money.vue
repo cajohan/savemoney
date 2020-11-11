3dd048254c8c4cde9d3d26c248a38abe9f5a263f
huangdi <huangdi_c@163.com>
2020-11-11 21:47:09 +0800

    所有 computed 改成get函数

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -22,13 +22,11 @@ import FormItem from '@/components/Money/FormItem.vue';
 
 @Component({
   components: {FormItem, Tags, Types, NumberPad},
-  computed: {
-    recordList() {
-      return this.$store.state.recordList;
-    }
-  }
 })
 export default class Money extends Vue {
+  get recordList(){
+    return this.$store.state.recordList;
+  }
   record: RecordItem = {
     tags: [], notes: '', type: '-', amount: 0
   };
